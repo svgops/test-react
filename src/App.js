0095d9c8c196de721c1c8bb1f43d8ef77c1f1391
svgops <64754167+svgops@users.noreply.github.com>
2020-05-08 13:37:30 -0600

    change-svg
    add a small rectangle to the hook svg when drawing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 // check this blog
 // https://blog.koenvangilst.nl/react-hooks-with-canvas/
 
+// drawing on the canvas with this hook-svg
 const HOOK_SVG =
   "m129.03125 63.3125c0-34.914062-28.941406-63.3125-64.519531-63.3125-35.574219 0-64.511719 28.398438-64.511719 63.3125 0 29.488281 20.671875 54.246094 48.511719 61.261719v162.898437c0 53.222656 44.222656 96.527344 98.585937 96.527344h10.316406c54.363282 0 98.585938-43.304688 98.585938-96.527344v-95.640625c0-7.070312-4.640625-13.304687-11.414062-15.328125-6.769532-2.015625-14.082032.625-17.960938 6.535156l-42.328125 64.425782c-4.847656 7.390625-2.800781 17.3125 4.582031 22.167968 7.386719 4.832032 17.304688 2.792969 22.160156-4.585937l12.960938-19.71875v42.144531c0 35.582032-29.863281 64.527344-66.585938 64.527344h-10.316406c-36.714844 0-66.585937-28.945312-66.585937-64.527344v-162.898437c27.847656-7.015625 48.519531-31.773438 48.519531-61.261719zm-97.03125 0c0-17.265625 14.585938-31.3125 32.511719-31.3125 17.929687 0 32.511719 14.046875 32.511719 31.3125 0 17.261719-14.582032 31.3125-32.511719 31.3125-17.925781 0-32.511719-14.050781-32.511719-31.3125zm0 0";
 
@@ -20,6 +21,8 @@ function draw(ctx, location) {
   ctx.scale(SCALE, SCALE);
   ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET);
   ctx.fill(HOOK_PATH);
+  ctx.fillRect(location.x, location.y, 100, 100);
+  ctx.strokeRect(location.x, location.y, 50, 50);
   ctx.restore();
 }
 
@@ -49,40 +52,7 @@ function usePersistentCanvas() {
 }
 
 export default function App() {
-  /*
-  const [locations, setLocations] = React.useState(
-    JSON.parse(localStorage.getItem("draw-app")) || []
-  );
-  */
-  //const [locations, setLocations] = usePersistentState([]);
   const [locations, setLocations, canvasRef] = usePersistentCanvas();
-
-  // create the react hook
-  // canvas is different from other DOM elements
-  // that is the reason you need a ref to the canvas
-  // for later update
-  //const canvasRef = React.useRef(null);
-  // another hook for the states
-  //const [locations, setLocations] = React.useState([]);
-
-  // add the third hook
-  // drawing on the canvas is a side-efect from the states
-  /*
-  React.useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    // everytime to draw anything, clear the canvas first
-    ctx.clearRect(0, 0, window.innerHeight, window.innerWidth);
-    // enumerate every location of click, draw a hook for each
-    locations.forEach(location => draw(ctx, location));
-  });
-  */
-  // draw is called inside the useEffect() for each click
-  /*
-  React.useEffect(() => {
-    localStorage.setItem("draw-app", JSON.stringify(locations));
-  });
-*/
 
   function handleCanvasClick(e) {
     const newLocation = { x: e.clientX, y: e.clientY };
